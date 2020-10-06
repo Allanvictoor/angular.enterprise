d9d066750694d952c3820f3041f964ecd52cc2e9
vivaweb <allanbatista@vivaweb.net>
2020-10-06 16:52:31 -0300

    configurando rota e adicionando novo component

--- agatha-angular/src/app/app-routing.module.ts
+++ agatha-angular/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProcedimentosComponent } from './procedimentos/procedimentos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: 'procedimentos', component: ProcedimentosComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
